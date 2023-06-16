2ade7c95a2755bc08f09a9e3d2d204d749139803
ASUS <siddharth041>
2023-06-16 16:11:47 +0530

    updated index.js

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function checkEqual(a, b) {
 let cheEq = checkEqual(1, 2);
 console.log(cheEq);
 
+
+function checkSign(num) {
+  let check = num > 0 ? "positive" 
+  : num < 0 ? "negative" 
+  : num == 0 ? "zero" 
+  : num === "" ? "Its a string, not a number" //this line is not working
+  : "Empty space";
+  return check;
+}
+
+let res = checkSign();
+console.log(res);
