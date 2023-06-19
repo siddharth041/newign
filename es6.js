23aa4b2da19cb893abeb258eb769e6f1e1fe8ff4
ASUS <siddharth041>
2023-06-19 17:55:34 +0530

    Added some new code

--- es6.js
+++ es6.js
@@ -85,3 +85,18 @@ const {today, tomorrow} = HIGH_TEMPERATURES;
 // const tomorrow = HIGH_TEMPERATURES.tomorrow;
 
 // Only change code above this line
+
+//Use Destructuring Assignment to Assign Variables from Objects
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+const{today:highToday, tomorrow:highTomorrow} = HIGH_TEMPERATURES;
+// const highToday = HIGH_TEMPERATURES.today;
+// const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+
+// Only change code above this line
